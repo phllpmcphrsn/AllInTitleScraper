8725d6e6a0e8e1cc8a7b8c5193ba792eeef1063c
Phillip McPherson <pmcpherson@luf.co>
2022-11-26 01:18:28 -0500

    Finish conditional formatting. Add streamlit objects

--- allintitle_scraper.py
+++ allintitle_scraper.py
@@ -2,6 +2,7 @@ import time
 import requests
 import pandas as pd
 import random
+import streamlit as st
 
 from bs4 import BeautifulSoup
 
@@ -27,43 +28,63 @@ def extract_keywords(filename) -> list:
         print('Column not found: "keyword"')
         return
     return keyword_df["keyword"].tolist() 
-    
 
+def format_cells(kgr):
+    # conditional formatting of kgr column (Series)
+    color = ''
+    if kgr < 0.25:
+        color = 'background-color: green'
+    elif kgr >= 0.25 and kgr < 1:
+        color = 'background-color: yellow'
+    else:
+        color = 'background-color: red'
+    return color
 
 # main
 # setup request
-headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
-stats = []
-keywords = extract_keywords('resources\\input\\allintitle.xlsx')
-exported_df = pd.DataFrame()
-exported_df.columns = ['Keyword', 'All In Title', 'Search Volume', 'KGR']
+with st.echo(code_location='below'):
+    uploaded_file = st.file_uploader(
+        "",
+        key="1",
+        help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
+    )
+    if uploaded_file is not None:
+        file_container = st.expander("Check your uploaded .csv")
+        keywords = extract_keywords(uploaded_file)
+        uploaded_file.seek(0)
+        file_container.write(keywords)
+    else:
+        st.info(f"""👆 Upload a .csv file first.""")
+        st.stop()
+        
+    headers={'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+    stats = []
+    exported_df = pd.DataFrame({'Keyword': [], 'All In Title': [], 'Search Volume': [], 'KGR': []})
 
-for i in keywords:
-    search_phrase = 'allintitle:' + i
-    params = {'q': search_phrase}
-    url='https://www.google.com/search'
+    for i in keywords:
+        search_phrase = 'allintitle:' + i
+        params = {'q': search_phrase}
+        url='https://www.google.com/search'
 
-    # perform request
-    response = requests.get(url, headers=headers, params=params)
+        # perform request
+        response = requests.get(url, headers=headers, params=params)
 
-    # parse response
-    soup = BeautifulSoup(response.text, 'html.parser')
-    result = soup.find("div", {"id":"result-stats"}).text
+        # parse response
+        soup = BeautifulSoup(response.text, 'html.parser')
+        result = soup.find("div", {"id":"result-stats"}).text
 
-    # assuming Google keeps the number of results in the following format:
-    # "About <number> results (<time>)"
-    # have to remove commas from string to make int
-    # should revert back to string for export
-    num_of_results = result.split()[1].replace(',','')
+        # assuming Google keeps the number of results in the following format:
+        # "About <number> results (<time>)"
+        # have to remove commas from string to make int
+        # should revert back to string for export
+        num_of_results = result.split()[1].replace(',','')
 
-    # adding a random number for volume
-    volume = random.randrange(1,1000)
-    kgr = round(num_of_results/volume, 2)
-    exported_df.loc[len(exported_df.index)] = [i, num_of_results, volume, kgr]
-    # stats[i] = [int(num_of_results.replace(',','')), ]
-    time.sleep(1)
-print(exported_df)
-
-# conditional formatting of kgr column (Series)
-green = [1 if kgr < 0.25 else 0 for kgr in kgrs ]
-yellow = [2 if kgr >= 0.25 or kgr < 1 else 0 for kgr in kgrs ]
+        # adding a random number for volume
+        volume = random.randrange(1,2)
+        kgr = round(int(num_of_results)/volume, 2)
+        exported_df.loc[len(exported_df.index)] = [i, num_of_results, volume, kgr]
+        # stats[i] = [int(num_of_results.replace(',','')), ]
+        time.sleep(1)
+        
+    exported_df.style.applymap(format_cells, subset=['KGR']).to_excel('resources\\output\\allintitle.xlsx', index=False, engine='openpyxl')
+    st.table(exported_df)
